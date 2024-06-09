0822373202f2bf3b77b3cefde0c595f78e54fd51
ssc1099 <ssc1099.csmm@gmail.com>
2024-06-09 19:29:50 +0200

    feat: website structure updated (urls and templates)

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload-invoices/', views.upload_invoices, name='upload_invoices'),
-    path('invoice-totals/', views.invoice_totals, name='invoice_totals'),
+    path('', views.index, name='index'),
+    path('invoices/', views.invoices, name='invoices'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,9 +3,28 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from app.forms import UploadFileForm
-from .models import Total
+from .models import Invoice, Total
 from django.conf import settings
 from . import tasks
+
+def invoices(request):
+    invoices = Invoice.objects.all()
+    return render(request, 'invoices.html', {'invoices': invoices})
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        file = request.FILES['file']
+        file_path = os.path.join(settings.MEDIA_ROOT, file.name)
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        tasks.process_invoices.delay(file_path)
+        return redirect('index')
+
+    totals = Total.objects.all()
+    form = UploadFileForm()
+    return render(request, 'index.html', {'form': form, 'totals': totals})
 
 # TODO to test
 def get_invoice_totals(request):
@@ -13,10 +32,6 @@ def get_invoice_totals(request):
     return JsonResponse(list(totals), safe=False)
 
 # TODO to test
-def invoice_totals(request):
-    totals = Total.objects.values('revenue_source', 'total_value', 'total_advance', 'total_expected_fee')
-    return render(request, 'invoice_totals.html', {'totals': totals})
-
 @csrf_exempt
 def post_upload_invoices(request):
     if request.method == 'POST' and request.FILES.get('file'):
@@ -28,17 +43,3 @@ def post_upload_invoices(request):
         tasks.process_invoices.delay(file_path)
         return JsonResponse({'status': 'success'})
     return JsonResponse({'status': 'fail'}, status=400)
-
-@csrf_exempt
-def upload_invoices(request):
-    if request.method == 'POST' and request.FILES.get('file'):
-        file = request.FILES['file']
-        file_path = os.path.join(settings.MEDIA_ROOT, file.name)
-        with open(file_path, 'wb+') as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
-        tasks.process_invoices.delay(file_path)
-        return redirect('invoice_totals')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload_invoices.html', {'form': form})
